7ab38dc9855a03ee07c60f9624379d41a7bfc169
Nikolai <niklonberg@gmail.com>
2024-05-18 10:10:05 +0200

    bring in auth0 module

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const indexRouter = require("./routes/index.js");
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER,
+};
 
 const app = express();
 app.set("views", "views");
@@ -10,6 +20,9 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")); // anything in public directory will be served before the express app tries to resolve routes
+// add authentication middleware
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 app.use("/", indexRouter);
 
