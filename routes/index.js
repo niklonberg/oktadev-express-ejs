fcefb7bc37b62294e61b15c078e7eb9d22ae5601
Nikolai <niklonberg@gmail.com>
2024-05-18 10:10:40 +0200

    add log of oidc.isAuthenticated for testing

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // set up get route,
 router.get("/", (req, res) => {
+  console.log(req.oidc.isAuthenticated());
   // render the index view when a get request is made to '/'
   res.render("index", { title: "Express" });
 });
