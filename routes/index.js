2dc49a3379649462138599ec0a94801c29f21a28
Nikolai <niklonberg@gmail.com>
2024-05-18 12:37:07 +0200

    use requiresAuth to ensure secured page can only be accessed if the user is logged in

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { requiresAuth } = require("express-openid-connect");
 
 // set up get route,
 router.get("/", (req, res) => {
@@ -12,7 +13,10 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/secured", (req, res) => {
+// in order to ensure this page cannot be rendered unless we are logged in,
+// we can provide an extra parameter to this endpoint handler to require that authentication
+// see line 3
+router.get("/secured", requiresAuth(), (req, res) => {
   res.render("secured", {
     title: "Secure page",
     isAuthenticated: req.oidc.isAuthenticated(),
