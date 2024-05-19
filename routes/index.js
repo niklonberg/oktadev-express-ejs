c648e2c32e32237de22c193485defb732b927b4f
Nikolai <niklonberg@gmail.com>
2024-05-19 09:59:18 +0200

    get access tokens, set authorization headers

--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,14 @@ router.get("/", (req, res) => {
 router.get("/secured", requiresAuth(), async (req, res) => {
   let data = {};
 
+  const { token_type, access_token } = req.oidc.accessToken;
+
   try {
-    const apiResponse = await axios.get("http://localhost:5000/public");
+    const apiResponse = await axios.get("http://localhost:5000/private", {
+      headers: {
+        authorization: `${token_type} ${access_token}`,
+      },
+    });
     data = apiResponse.data;
   } catch (e) {} // skip error handling, could be added later
 
