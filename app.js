88ca2cc75dba5ac8d25ef3523b2369f82e90df35
Nikolai <niklonberg@gmail.com>
2024-05-19 10:03:48 +0200

    set clientSecret, authorizationParams

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const config = {
   baseURL: process.env.BASEURL,
   clientID: process.env.CLIENTID,
   issuerBaseURL: process.env.ISSUER,
+  clientSecret: process.env.CLIENTSECRET,
+  authorizationParams: {
+    response_type: "code",
+    audience: "http://localhost:5000",
+    scope: "openid profile email",
+  },
 };
 
 const app = express();
